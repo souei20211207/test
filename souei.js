dfcfa298d852e27b76947c0a5c28afbaf8c10207
souei20211207 <160836999+souei20211207@users.noreply.github.com>
2024-03-07 00:10:24 +0900

    20240307

--- souei.js
+++ souei.js
@@ -61,5 +61,8 @@ function f3(){
 
 function f4(){
 	kkk=document.querySelector("#main > div > div.layout.layout-content.touch-no.justify-center.align-start > div.layout.app-left.column.justify-start.align-end.wrap > div.layout.frame.shrink.column.justify-space-between.align-content-space-between > div > div.layout.member-box.column.justify-start.align-content-center > button > span > span");
-	console.log(kkk);
+	if(String(kkk)=="null"){
+		document.querySelector("#main > div > div.layout.layout-content.touch-no.justify-center.align-start > div.layout.app-left.column.justify-start.align-end.wrap > div.layout.frame.shrink.column.justify-space-between.align-content-space-between > div > div.layout.regist.mt-0.column.justify-start.align-content-center > button").click();
+	};
+	window.location.href="https://www.redstoneonline.jp/signin/1?returnurl=https%3A%2F%2Fwww.redstoneonline.jp%2F";
 };
